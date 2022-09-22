2f1c6251f36c8e0def3be9a1abce92452c500154
sophie <soph_lew@hotmail.co.uk>
2022-09-22 15:50:42 +0100

    refactored code that decreased quality of 'other items' by 1 each day

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,14 +13,13 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.isItemOther(this.items[i])) {
+        console.log("hello 1")
             this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
+            console.log("hello 2")
       } else {
         if (this.items[i].quality < 50) {
+          console.log("hello 3")
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
@@ -59,6 +58,10 @@ class Shop {
     }
 
     return this.items;
+  }
+
+  isItemOther(item) {
+    return item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros'
   }
 }
 
