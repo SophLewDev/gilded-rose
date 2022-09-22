6a9021ef3978e562342abcc3b89c261dd88843f3
sophie <soph_lew@hotmail.co.uk>
2022-09-22 14:39:11 +0100

    modified given test to return name, sellIn, and quality values of item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,11 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("return item name", function() {
+  it("return item name, sell in date, and quality value", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
+    const items = gildedRose.items
     expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(0);
   });
 });
